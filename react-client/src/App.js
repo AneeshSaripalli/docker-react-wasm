4d7f70f2da3a3f86282cdfb88ac3d44ac061733d
Aneesh Saripalli <aneeshsaripalli@gmail.com>
2020-07-05 12:05:25 -0500

    Basic React, NGINX, and Rust WASM.

---
+++ react-client/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+//@ts-ignore
+import Wasm from "react-wasm";
+
+function App() {
+  console.log("TEST HEawdawRE")
+  return (
+    <div className="App">
+      <Wasm url="/wasm/wasm_bg.wasm">
+        {(info) => {
+          if (info.loading) return "Loading...";
+          if (info.error) return "An error has occurred";
+
+          const { module, instance } = info.data;
+          console.log(instance.exports.fib(15))
+          return <div>1 + 2 = {instance.exports.add(1, 2)}</div>;
+        }}
+      </Wasm>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
